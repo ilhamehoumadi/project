981e3c3a7cac99352d2dd6f7d22306df645dbc18
ilhame <ilhamehoumadi15@gmail.com>
2024-04-15 12:41:50 +0100

    login

--- project/pages/views.py
+++ project/pages/views.py
@@ -12,17 +12,17 @@ def index(request):
     #template=loader.get_template('pages/index.html')
     #return HttpResponse( template.render )
 
-def signup(request):
+def register(request):
     
     form = CreateUserForm()
     if request.method =="POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("index.html")
+            return redirect("login")
         
-    context = { 'signupform':form}
-    return render(request ,'account/signup.html',context=context)
+    context = { 'registerform':form}
+    return render(request ,'account/register.html',context=context)
 
 def login(request):
     form=LoginForm()
@@ -35,33 +35,24 @@ def login(request):
             user=authenticate(request,username=username,password=password)
             if user is not None:
                 login(request,user)
-                return redirect("account/dashboard.html")
+                return redirect("'account/dashboard.html'")
     context= { 'LoginForm':form}
     return render(request, 'account/login.html', context=context)
 def dashboard(request):
     return render(request ,'account/dashboard.html')
        
     
-#def signup(request):
- #   return render(request ,'account/signup.html')
+def signup(request):
+    return render(request ,'account/signup.html')
 
 def association(request):
-    #return render(request ,'account/association.html')
-    form = CreateUserForm()
-    if request.method =="POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("index.html")
-        
-    context = { 'signupform':form}
-    return render(request ,'account/signup.html',context=context)
+    return render(request ,'account/association.html')
     
 def logout(request):
     
     # Call Django's logout function
     django_logout(request)
-    return redirect("/")
+    return redirect("")
     #return render(request ,'account/logout.html')
     
 #def about(request):
